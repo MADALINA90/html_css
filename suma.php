858cba89f1da0dbec34d53c2bb088044beee0d72
MADALINA90 <45117813+MADALINA90@users.noreply.github.com>
2018-11-22 20:42:23 +0200

    yu

--- suma.php
+++
@@ -1,8 +0,0 @@
-<?php
-  echo 'Introduceti primul numar:';
-  $a = readline();
-  echo 'Introduceti al doilea numar:';
-  $b = readline();
-  $sum = $a + $b;
-  echo "Suma este: $sum ".PHP_EOL;
- ?>
